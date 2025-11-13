e5936d70f6bfe4a2d94a907c6c1eb0dcbc13ccfe
hyland-uw <protonk@gmail.com>
2025-11-12 18:30:07 -0800

    add AGENTS.md
    also add another unmonitored pipeline!

---
+++ R/ToTransform/diagnostics.R
@@ -0,0 +1,32 @@
+nframes   <- 400
+duration  <- 2
+fps       <- nframes / duration    # 100
+out_file  <- "scatter.mp4"
+
+generator_vis <- function(draw = 200, x_max = 16) {
+  x <- frsr_sample(n = draw, method = "log_stratified", x_max = x_max)$input
+  y <- frsr_sample(n = draw, method = "irrational",      x_max = x_max)$input
+  par(mai = c(0,0,0,0))  # no margins
+  smoothScatter(x = y, y = x, xlim = rev(range(y)), nbin = 64,
+                axes = FALSE, ann = FALSE, asp = 1)
+}
+
+av::av_capture_graphics(
+  {
+    for (i in seq_len(nframes)) {
+      generator_vis()
+    }
+  },
+  output    = out_file,
+  framerate = fps,
+  width     = 800,   # square frames to match asp=1
+  height    = 800,
+  res       = 144
+)
+
+# Preview in RStudio, else open in default player
+if (requireNamespace("rstudioapi", quietly = TRUE) && rstudioapi::isAvailable()) {
+  rstudioapi::viewer(out_file)
+} else {
+  utils::browseURL(out_file)
+}
